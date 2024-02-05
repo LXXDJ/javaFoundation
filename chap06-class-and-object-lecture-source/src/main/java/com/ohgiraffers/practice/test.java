93d54cc6a3644a1abf0f45dc57f57502df449a57
LXXDJ <joker96.dj@gmail.com>
2024-02-05 12:29:06 +0900

    dd

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/practice/test.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/practice/test.java
@@ -1,9 +1,24 @@
 package com.ohgiraffers.practice;
 
+import java.util.Scanner;
+
 public class test {
-    public static void main(String[] args) {
+    // private 으로 필드 직접 접근 제한
+    private String kinds;
+    private int hp;
 
-        
-
+    // public 메소드를 이용해 간접 접근 허용
+    public void setKinds(String kinds){
+        this.kinds = kinds;
+    }
+    public void setHp(int hp){
+        if(hp>0){           // 검증하는 로직 추가
+            this.hp = hp;
+        }else{
+            this.hp = 0;
+        }
+    }
+    public String getInfo(){
+        return "몬스터의 종류는 "+this.kinds+"이고, 체력은 "+this.hp+"입니다.";
     }
 }
---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section04.dto;
+
+import com.ohgiraffers.section04.constructor.User;
+
+public class Application {
+    public static void main(String[] args) {
+        /* [생성자를 이용한 초기화]
+        *  장점 : setter 메소드를 여러번 호출해서 사용하지 않고 단 한번의 호출로 인스턴스를 생성 및 초기화 가능
+        *  단점 : 필드를 초기화 할 매개변수의 갯수를 경우의 수 별로 모두 만들어둬야 함
+        *        호출 시 인자가 많아지는 경우 어떠한 값이 어떤 필드를 의미하는지 한눈에 보기 힘듦
+        *
+        *  [설정자를 이용한 초기화]
+        *  장점 : 필드를 초기화하는 각각의 값들이 어던 필드를 초기화하는지 명확하게 볼 수 있다.
+        *  단점 : 하나의 인스턴스를 생성할 때 한번의 호출로 끝나지 않음
+        *  */
+
+        /* 생성자를 이용한 초기화 */
+        UserDTO user1 = new UserDTO("ohgiraffers", "ohgiraffers", "ohgiraffers", new java.util.Date());
+
+
+    }
+}
