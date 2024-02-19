382af60a378060c7b8c275981015eb2e71b692e1
LXXDJ <joker96.dj@gmail.com>
2024-02-19 21:19:48 +0900

    dd

--- Practice-collection/src/main/java/com/ohgiraffers/home/comparator/AscBookTitle.java
+++ Practice-collection/src/main/java/com/ohgiraffers/home/comparator/AscBookTitle.java
@@ -5,7 +5,6 @@ import com.ohgiraffers.home.model.dto.BookDTO;
 import java.util.Comparator;
 
 public class AscBookTitle implements Comparator<BookDTO> {
-
     @Override
     public int compare(BookDTO o1, BookDTO o2) {
         return o1.getTitle().compareTo(o2.getTitle());
